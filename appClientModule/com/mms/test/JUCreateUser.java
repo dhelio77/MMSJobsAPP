765d16e9ad19aa5b47e466153248687d6083481a
Rodelio Ancheta <rhodel.ancheta@gmail.com>
2015-02-24 22:43:20 +1100

    Modify classpaths

--- appClientModule/com/mms/test/JUCreateUser.java
+++ appClientModule/com/mms/test/JUCreateUser.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.mms.service.IAccessAcctService;
